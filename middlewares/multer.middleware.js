01081be681a8e9415f72ec9f58ae3fce2e278f93
iamumesh2058 <umesh.grg2058@gmail.com>
2025-03-09 09:29:38 +0545

    Implementing multer to work with images

---
+++ middlewares/multer.middleware.js
@@ -0,0 +1,41 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let filepath = 'public/uploads';
+        if (!fs.existsSync(filepath)) {
+            fs.mkdirSync(filepath, { recursive: true });
+        }
+        cb(null, filepath);
+    },
+    filename: (req, file, cb) => {
+        const extension = path.extname(file.originalname);
+        let filename = path.basename(file.originalname, extension);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+
+        filename += '-' + uniqueSuffix + extension;
+
+        cb(null, filename);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (!file.originalname.match(/[.](jpeg|JPEG|png|PNG|gif|GIF|svg|SVG)$/)) {
+        return cb(new Error("Invalid image format"), false);
+    }
+    cb(null, true);
+}
+
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 2000000
+    }
+});
+
+
+module.exports = upload;

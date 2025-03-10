fc035a8d73eb074af746df909ab7dd58f315a509
iamumesh2058 <umesh.grg2058@gmail.com>
2025-03-10 09:39:31 +0545

    Implementing order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,5 @@
 const Order = require("../models/order.model");
-const OrderItems = require("../models/category.model");
+const OrderItems = require("../models/order.items.model");
 
 const { BadRequestError, NotFoundError } = require("../errors/custom.error");
 
@@ -27,7 +27,7 @@ exports.placeOrder = async (req, res) => {
     // calculate individuals totals
     const individual_total = await Promise.all(
         orderItemsIds.map(async (orderItem) => {
-            const order_item = await OrderItems.findById(orderItem).populate('product', 'price');
+            const order_item = await OrderItems.findById(orderItem).populate("product", "price");
             return order_item.product.price * order_item.quantity;
         })
     );
@@ -49,9 +49,65 @@ exports.placeOrder = async (req, res) => {
 // GET ALL ORDERS
 exports.getAllOrders = async (req, res) => {
     const orders = await Order.find()
-        .populate('user', 'username')
+        .populate("user", "username")
         .populate({ path: "orderItems", populate: ({ path: "product", poupulate: ("category") }) });
 
+    if (!orders) throw new NotFoundError("Orders not found");
+    res.status(200).json({ orders });
+}
+
+
+// GET ORDER DETAILS
+exports.getOrderDetails = async (req, res) => {
+    const order = await Order.findById(req.params.orderId)
+        .populate("user", "username")
+        .populate({ path: "orderItems", populate: ({ path: "product", poupulate: ("category") }) });
+    if (!order) throw new NotFoundError(`Order with id ${req.params.orderId} not found`);
+    res.status(200).json({ order });
+}
+
+
+// GET ORDER OF A PARTICULAR USER
+exports.getUserOrders = async (req, res) => {
+    const orders = await Order.find({ user: req.user.userId })
+        .populate("user", "username")
+        .populate({ path: "orderItems", populate: ({ path: "product", populate: ("category") }) });
+    if (!orders) throw new NotFoundError("User has not ordered yet");
+    res.status(200).json({ orders });
+}
+
+
+// GET ORDER BY STATUS
+exports.getOrdersByStatus = async (req, res) => {
+    const orders = await Order.find({ status: req.query.status })
+        .populate("user", "username")
+        .populate({ path: "orderItems", populate: ({ path: "product", populate: ("category") }) });
     if (!orders) throw new NotFoundError("No orders");
     res.status(200).json({ orders });
+}
+
+
+// UPDATE ORDER STATUS
+exports.updateOrderStatus = async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.orderId,
+        req.body,
+        { new: true }
+    );
+    if (!order) throw new NotFoundError(`No order with id ${req.params.orderId}`);
+    res.status(200).json({ msg: "Order updated successfully" });
+}
+
+
+// DELETE ORDER
+exports.deleteOrder = async (req, res) => {
+    const order = await Order.findById(req.params.orderId);
+    if (!order) throw new NotFoundError(`No order with id ${req.params.orderId}`);
+    await Promise.all(
+        order.orderItems.map(async (item) => {
+            const orderItem = await OrderItems.findByIdAndDelete(item);
+            if (!orderItem) throw new NotFoundError("Order item not found");
+        })
+    );
+    res.status({ msg: "Order deleted successfully" });
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -3,12 +3,22 @@ const router = express.Router();
 
 const {
     placeOrder,
-    getAllOrders
+    getAllOrders,
+    getOrderDetails,
+    getUserOrders,
+    getOrdersByStatus,
+    updateOrderStatus,
+    deleteOrder
 } = require("../controllers/order.controller");
-const { authenticateUser } = require("../middlewares/auth.middleware");
+const { authenticateUser, authorizePermissions } = require("../middlewares/auth.middleware");
 
-router.post("/placeOrder", authenticateUser, placeOrder);
-router.get("/getAllOrders", getAllOrders);
+router.post("/placeOrder", authenticateUser, authorizePermissions('customer'), placeOrder);
+router.get("/getAllOrders", authenticateUser, authorizePermissions('admin'), getAllOrders);
+router.get("/getOrderDetails/:orderId", authenticateUser, getOrderDetails);
+router.get("/getUserOrders", authenticateUser, authorizePermissions("customer"), getUserOrders);
+router.get("/getOrderByStatus", authenticateUser, getOrdersByStatus);
+router.put("/updateOrderStatus/:orderId", authenticateUser, updateOrderStatus);
+router.delete("/deleteOrder/:orderId", authenticateUser, deleteOrder);
 
 
 module.exports = router;
